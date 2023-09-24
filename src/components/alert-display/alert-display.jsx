215d405ad73fe439e8a26689fe52a426b3d540f7
Lucas Dantas <lucasdantasguitar@gmail.com>
2023-09-24 20:57:29 -0300

    update paragraph in alert display

--- src/components/alert-display/alert-display.jsx
+++ src/components/alert-display/alert-display.jsx
@@ -8,7 +8,7 @@ export default function AlertDisplay() {
             <h2>Desafio</h2>
             <LogoFortnite/>
             <img src="./dants.dev.svg" alt="Logo Dants Dev" width={100} />
-            <p>VISUALIZAÇÂO DISPONIVEL APENAS PARA DESKTOP OU TELA CHEIA</p>
+            <p>VISUALIZAÇÃO DISPONIVEL APENAS PARA DESKTOP OU TELA CHEIA</p>
             <a href="https://github.com/ldantsc/desafio-fortnite" target="_blank">
                 <p style={{ textDecoration: "underline" }}>Repositório do projeto</p>
             </a>
